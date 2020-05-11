105533d1399bf56f30bf4e3bd6fb53e49bc7bf41
Trung Dang <2017.12ch.34.dangductrung@gmail.com>
2020-05-12 02:24:51 +0700

    [login] add UI

---
+++ app/src/main/java/com/adida/chatapp/login/login_page.java
@@ -0,0 +1,18 @@
+package com.adida.chatapp.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.adida.chatapp.R;
+
+public class login_page extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+
+
+    }
+}
