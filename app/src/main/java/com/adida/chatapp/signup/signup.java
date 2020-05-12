1af08aed342440c68a485998d2ac21675fbd88a7
Trung Dang <2017.12ch.34.dangductrung@gmail.com>
2020-05-12 19:02:37 +0700

    [signup] add UI

---
+++ app/src/main/java/com/adida/chatapp/signup/signup.java
@@ -0,0 +1,16 @@
+package com.adida.chatapp.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.adida.chatapp.R;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
