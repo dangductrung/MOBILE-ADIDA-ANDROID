55280d7b32b07aa1cd56fe7b5c6c5e3b13b474c7
Trung Dang <2017.12ch.34.dangductrung@gmail.com>
2020-05-14 01:23:40 +0700

    [firebase-auth] add firebase auth

--- app/src/main/java/com/adida/chatapp/signup/SignupPage.java
+++ app/src/main/java/com/adida/chatapp/signup/SignupPage.java
@@ -1,23 +1,36 @@
 package com.adida.chatapp.signup;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.adida.chatapp.R;
+import com.adida.chatapp.stringhelper.StringHelper;
+import com.adida.chatapp.stringkeys.StringKeys;
 import com.adida.chatapp.login.LoginPage;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignupPage extends Activity implements View.OnTouchListener {
+    private FirebaseAuth mAuth;
+
     EditText username, password, repassword;
-    TextView gotoLogin;
+    TextView gotoLogin, errorMessage;
     Button registerButton;
     Intent gotoLoginIntent;
 
@@ -27,11 +40,23 @@ public class SignupPage extends Activity implements View.OnTouchListener {
         setContentView(R.layout.activity_signup);
         gotoLoginIntent = new Intent(this, LoginPage.class);
         this.setup();
+
+//        SharedPreferences sharedPref = getSharedPreferences(StringKeys.SPrefName, MODE_PRIVATE);
+//        String test = sharedPref.getString("uuid", null);
+
     }
 
     private void setup() {
+        mAuth = FirebaseAuth.getInstance();
         this.connectToComponent();
         this.setupKeyboard();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
     }
 
     private void connectToComponent() {
@@ -40,7 +65,48 @@ public class SignupPage extends Activity implements View.OnTouchListener {
         repassword = (EditText) findViewById(R.id.repasswordRegisterEditText);
         registerButton = (Button) findViewById(R.id.registerButton);
         gotoLogin = (TextView) findViewById(R.id.goToLogin);
+        errorMessage = (TextView) findViewById(R.id.errorEmail);
+
         gotoLogin.setOnTouchListener((View.OnTouchListener) this);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerButtonClicked();
+            }
+        });
+    }
+
+    private void registerButtonClicked() {
+        if(!checkValidate()) {
+            showInvalidEditTField();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(username.getText().toString(), password.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d("", "createUserWithEmail:success");
+                    startActivity(gotoLoginIntent);
+                } else {
+                    Log.w("", "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(SignupPage.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                    showInvalidEditTField();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        errorMessage.setVisibility(View.GONE);
+        final int sdk = android.os.Build.VERSION.SDK_INT;
+        if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            errorMessage.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.edit_text_border) );
+        } else {
+            errorMessage.setBackground(ContextCompat.getDrawable(this, R.drawable.edit_text_border));
+        }
     }
 
     private void setupKeyboard() {
@@ -80,5 +146,19 @@ public class SignupPage extends Activity implements View.OnTouchListener {
             startActivity(gotoLoginIntent);
         }
         return false;
+    }
+
+    public boolean checkValidate() {
+        return StringHelper.isEmailValid(errorMessage.getText().toString());
+    }
+
+    public void showInvalidEditTField() {
+        errorMessage.setVisibility(View.VISIBLE);
+        final int sdk = android.os.Build.VERSION.SDK_INT;
+        if(sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            errorMessage.setBackgroundDrawable(ContextCompat.getDrawable(this, R.drawable.edit_text_error_border) );
+        } else {
+            errorMessage.setBackground(ContextCompat.getDrawable(this, R.drawable.edit_text_error_border));
+        }
     }
 }
