3fe771ef09a3c2420e3d6b0ce1bcbbfba4caf17e
Trung Dang <2017.12ch.34.dangductrung@gmail.com>
2020-05-20 01:19:01 +0700

    [profile + statistic] add statistic UI

---
+++ app/src/main/java/com/adida/chatapp/statistic/StatisticPage.java
@@ -0,0 +1,16 @@
+package com.adida.chatapp.statistic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.adida.chatapp.R;
+
+public class StatisticPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistic_page);
+    }
+}
