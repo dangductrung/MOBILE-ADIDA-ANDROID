ef413c7a06d0fe719ec773fd772443523d85cf47
Tran Thuan Thanh <t0942842441@gmail.com>
2020-06-14 18:18:11 +0700

    fix call & audio call

---
+++ app/src/main/java/com/adida/chatapp/callscreen/AudioCallScreenActivity.java
@@ -0,0 +1,120 @@
+package com.adida.chatapp.callscreen;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaRecorder;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.adida.chatapp.R;
+import com.adida.chatapp.entities.User;
+import com.adida.chatapp.extendapplication.ChatApplication;
+import com.adida.chatapp.webrtc_connector.RTCPeerConnectionWrapper;
+import com.adida.chatapp.webrtc_connector.Utils;
+
+import org.webrtc.AudioSource;
+import org.webrtc.AudioTrack;
+import org.webrtc.EglBase;
+import org.webrtc.MediaConstraints;
+import org.webrtc.PeerConnectionFactory;
+import org.webrtc.SurfaceTextureHelper;
+import org.webrtc.SurfaceViewRenderer;
+import org.webrtc.VideoCapturer;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
+
+public class AudioCallScreenActivity extends AppCompatActivity {
+    public static void open(Context context, String userId,boolean isAnswer) {
+        Intent actCall = new Intent(context, AudioCallScreenActivity.class);
+        actCall.putExtra("remoteUserId",userId);
+        actCall.putExtra("type",isAnswer);
+        context.startActivity(actCall);
+    }
+
+    public static void open(Context context, String userId,String sdp,boolean isAnswer) {
+        Intent actCall = new Intent(context, AudioCallScreenActivity.class);
+        actCall.putExtra("remoteUserId",userId);
+        actCall.putExtra("sdp",sdp);
+        actCall.putExtra("type",isAnswer);
+        context.startActivity(actCall);
+    }
+
+    private Button btnEndCall;
+
+    AudioTrack audioTrackLocal;
+    AudioTrack audioTrackRemote;
+
+    RTCPeerConnectionWrapper wrapper;
+    private String remoteUserId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_screen);
+
+        btnEndCall=findViewById(R.id.btnEndCall);
+
+        Bundle b = getIntent().getExtras();
+        String extrasUserId=b.getString("remoteUserId");
+        remoteUserId=extrasUserId;
+        String sdp=b.getString("sdp");
+        boolean isAnswer=b.getBoolean("type");
+
+        createLocalAudioTrack();
+
+        if(!ChatApplication.getInstance().getUserPeerConnections().containsKey(extrasUserId)){
+            wrapper= new RTCPeerConnectionWrapper(extrasUserId,this);
+            wrapper.startDataChannel();
+            ChatApplication.getInstance().getUserPeerConnections().put(extrasUserId,wrapper);
+        }
+        else{
+            wrapper= ChatApplication.getInstance().getUserPeerConnections().get(extrasUserId);
+            wrapper.resetPeerConnection();
+            wrapper.startDataChannel();
+        }
+
+        btnEndCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    StopCall();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        wrapper.setCallContext(this);
+        wrapper.addTrack(audioTrackLocal);
+
+        if(isAnswer){
+            wrapper.setRemoteDescription(sdp);
+            wrapper.createAnswer();
+        }
+        else{
+            wrapper.createOffer();
+        }
+    }
+
+    private void createLocalAudioTrack(){
+        PeerConnectionFactory factory= ChatApplication.getInstance().getPeerConnectionFactory();
+        AudioSource audioSource=factory.createAudioSource(new MediaConstraints());
+        audioTrackLocal=factory.createAudioTrack("ARDAMSa0",audioSource);
+    }
+
+
+    public void StopCall() throws InterruptedException {
+        if(wrapper!=null){
+            if(audioTrackRemote!=null)
+                audioTrackRemote.setEnabled(false);
+        }
+        finish();
+    }
+}
+
--- app/src/main/java/com/adida/chatapp/webrtc_connector/RTCPeerConnectionWrapper.java
+++ app/src/main/java/com/adida/chatapp/webrtc_connector/RTCPeerConnectionWrapper.java
@@ -2,13 +2,16 @@ package com.adida.chatapp.webrtc_connector;
 
 import android.app.Activity;
 import android.content.Context;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.adida.chatapp.R;
+import com.adida.chatapp.callscreen.AudioCallScreenActivity;
 import com.adida.chatapp.callscreen.CallScreenActivity;
 import com.adida.chatapp.chatscreen.DefaultMessagesActivity;
 import com.adida.chatapp.entities.User;
@@ -23,16 +26,22 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.webrtc.AudioTrack;
 import org.webrtc.DataChannel;
 import org.webrtc.IceCandidate;
 import org.webrtc.MediaConstraints;
+import org.webrtc.MediaStream;
+import org.webrtc.MediaStreamTrack;
 import org.webrtc.PeerConnection;
+import org.webrtc.RtpSender;
+import org.webrtc.RtpTransceiver;
 import org.webrtc.SessionDescription;
 import org.webrtc.VideoTrack;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class RTCPeerConnectionWrapper {
@@ -46,6 +55,7 @@ public class RTCPeerConnectionWrapper {
     private Context chatContext;
     public int state;
     private Context callContext;
+    private MediaStream mediaStream;
 
     public  RTCPeerConnectionWrapper(String remoteUserID, Context activityContext){
         this.remoteUserID=remoteUserID;
@@ -72,7 +82,39 @@ public class RTCPeerConnectionWrapper {
         peerConnection.addTrack(cameraVideoTrack);
     }
 
-    public void StartDataChannel(){
+    public void addTrack(AudioTrack audioTrack){
+        peerConnection.addTrack(audioTrack);
+    }
+
+
+    public void removeTrack(VideoTrack track){
+        if(mediaStream!=null){
+            mediaStream.removeTrack(track);
+        }
+
+    }
+
+    public void removeTrack(AudioTrack track){
+        if(mediaStream!=null){
+            mediaStream.removeTrack(track);
+        }
+    }
+
+    public void removeStream(){
+        //peerConnection.removeStream(mediaStream);
+
+        List<RtpSender> transceivers= peerConnection.getSenders();
+        //transceivers.removeAll(transceivers);
+
+        Iterator iterator = transceivers.iterator();
+        while(iterator.hasNext()) {
+            RtpSender trans= (RtpSender) iterator.next();
+            trans.setTrack(null,true);
+        }
+
+    }
+
+    public void startDataChannel(){
         dataChannel=peerConnection.createDataChannel(remoteUserID,new DataChannel.Init());
         dataChannel.registerObserver(new DataChannel.Observer() {
             @Override
@@ -104,10 +146,12 @@ public class RTCPeerConnectionWrapper {
     }
 
     public void sendDataChannelMessage(String message){
-        message = SharePref.getInstance(activityContext).getUuid() + "-message-" + message;
-        ByteBuffer data = Utils.stringToByteBuffer(message, Charset.defaultCharset());
-        Log.d("send message", "sendDataChannelMessage: ");
-        dataChannel.send(new DataChannel.Buffer(data, false));
+        if(dataChannel!=null && dataChannel.state()== DataChannel.State.OPEN && message!=null && message!=""){
+            message = SharePref.getInstance(activityContext).getUuid() + "-message-" + message;
+            ByteBuffer data = Utils.stringToByteBuffer(message, Charset.defaultCharset());
+            Log.d("send message", "sendDataChannelMessage: ");
+            dataChannel.send(new DataChannel.Buffer(data, false));
+        }
     }
 
     public void sendImageUrlMessage(String url, String uniqueId) {
@@ -130,6 +174,11 @@ public class RTCPeerConnectionWrapper {
                 tempOfferSessionDescription= sessionDescription;
 
                 //peerConnection.setLocalDescription(new SimpleSdpObserver(), sessionDescription);
+            }
+
+            @Override
+            public void onCreateFailure(String s) {
+                Log.d("failed", "onCreateFailure: "+s);
             }
         }, sdpMediaConstraints);
     }
@@ -154,6 +203,9 @@ public class RTCPeerConnectionWrapper {
         //Prompt yes/no
         if(sdp.contains("m=video")){
             CallScreenActivity.open(activityContext,remoteUserID,sdp,true);
+        }
+        else if (sdp.contains("m=audio")){
+            AudioCallScreenActivity.open(activityContext,remoteUserID,sdp,true);
         }
         else{
             peerConnection.setRemoteDescription(new SimpleSdpObserver()
@@ -194,7 +246,7 @@ public class RTCPeerConnectionWrapper {
     }
 
     public void receiveDataChannelMessage(String message){
-        MainActivity mainActivity= (MainActivity)activityContext;
+        Activity mainActivity= (Activity)activityContext;
         mainActivity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -219,6 +271,12 @@ public class RTCPeerConnectionWrapper {
             }
         });
 
+    }
+
+    public void resetPeerConnection() {
+        peerConnection= ChatApplication.getInstance()
+                .getPeerConnectionFactory()
+                .createPeerConnection(getIceServers(),SimplePCObserver.getPCObserver(this));
     }
 
     private void getUserInfo(String uuid, String sendingMessage, int type) {
@@ -264,4 +322,15 @@ public class RTCPeerConnectionWrapper {
             }
         });
     }
+
+    public void receiveAddTrackMessage(AudioTrack audioTrack){
+        audioTrack.setVolume(1);
+        Log.d("123", "receiveAddTrackMessage: "+audioTrack.enabled());
+    }
+
+    public void getMediaStream(MediaStream mediaStream){
+        this.mediaStream=mediaStream;
+    }
+
+
 }
